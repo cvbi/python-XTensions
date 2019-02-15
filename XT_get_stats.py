83bcf910ef7f860abfee3656c54f8c2df7ea6cf6
nilesh-patil <nilesh5760@gmail.com>
2019-02-14 21:30:24 -0500

    Update XT_get_stats.py
    Updated Get all stats code.

--- XT_get_stats.py
+++ XT_get_stats.py
@@ -13,9 +13,9 @@
 #    </CustomTools>
 
 
+import os
 import time
 import ImarisLib
-import os
 
 from cvbi.base_imaris.objects import GetSurpassObjects
 from cvbi.gui import create_window_from_list, get_output_dir
@@ -26,9 +26,6 @@ from cvbi.base_imaris.stats import get_imaris_statistics
 
 def XT_GetStats(aImarisId):
 
-    vImarisLib = ImarisLib.ImarisLib()
-    vImaris = vImarisLib.GetApplication(aImarisId)
-
     print('''
     ####################################################################################
     ###########################     Extension started     ##############################
@@ -36,21 +33,27 @@ def XT_GetStats(aImarisId):
     ''')
     time.sleep(5)
 
+    vImarisLib = ImarisLib.ImarisLib()
+    vImaris = vImarisLib.GetApplication(aImarisId)
+    imaris_file = vImaris.GetCurrentFileName()
+    imaris_dir = os.path.dirname(imaris_file)
+    imaris_name = os.path.basename(imaris_file)
+
     object_type_list = ["spots", "surfaces", "filaments", "cells"]
-    object_type = create_window_from_list(object_list=object_type_list, window_title='Select one object type')
+    object_type = create_window_from_list(object_list=object_type_list,
+                                          window_title='Select one object type',
+                                          w=500, h=500)
 
     print('Object type Selected : '+object_type)
 
     objects = GetSurpassObjects(vImaris=vImaris, search=object_type)
     objects_list = objects.keys()
-    object_name = create_window_from_list(object_list=objects_list, window_title='Select one object')
+    object_name = create_window_from_list(object_list=objects_list,
+                                          window_title='Select one object',
+                                          w = 500, h = 50*len(objects_list))
     print('Object Selected : '+object_name)
 
     all_stats = get_imaris_statistics(vImaris=vImaris, object_type=object_type, object_name=object_name)
-
-    imaris_file = vImaris.GetCurrentFileName()
-    imaris_dir = os.path.dirname(imaris_file)
-    imaris_name = os.path.basename(imaris_file)
 
     output_dir = get_output_dir(initial_dir=imaris_dir)
     output_file = imaris_name+'_'+object_name+'_statistics.txt'
